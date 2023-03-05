7ffd97fed62bffc61876f3b148bc8087339f40cd
Jehiel-MW <olami.solomon@gmail.com>
2023-03-05 11:31:19 +0100

    100 days is starting today

---
+++ 100_days-python_program/day1.py
@@ -0,0 +1,9 @@
+#This program is to take input from the user's and concatenate it with some text in the print statement
+food = input("Enter a food name: ")
+plant = input("Plant name: ")
+cookM = input("Enter cooking method: ")
+badFood = input("what is a burned food described: ")
+diy = input("Enter a diy material: ")
+
+print("\n\nMenu")
+print(cookM, food, 'with', badFood, plant, 'on a bed of', diy)
